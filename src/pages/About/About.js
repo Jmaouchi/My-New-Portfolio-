8f9e362c30b594da948e2e81048db93914c483e6
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-07-22 23:53:11 -0500

    added about page

---
+++ src/pages/About/About.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './style.css'
+import { Link } from 'react-router-dom';
+import AnimatePage from '../../Components/animateB'
+function About() {
+  return (
+    <div className='contact-page'>
+      <AnimatePage>
+      <div className='contact-section'>
+        <div className='left-part'>
+          <div className='back'>
+            <Link to={'/'}><i className="fa-solid fa-backward"></i></Link>
+          </div>
+          <h4 className='heading-happiness'><span className='color'>Happ</span>iness</h4>
+          <p className='paragraph-left-part'><span className='color'>Do you want to build something that</span> will make your life easier!!</p>  
+          <p to={'/work'} className='paragraph-left-part-projects'><span className='color'>Check Out My</span> Projects First</p>
+
+          {/* social media      */}
+          <div className='scl-media-contact'>
+            <Link to={'https://www.facebook.com/djigo.mat.1'}><li className='facebook-contact'><i className="fab fa-facebook"></i></li></Link>
+            <Link to={'https://www.instagram.com/'}><li className='instagram-contact'><i className="fab fa-instagram"></i></li></Link>
+            <Link to={'https://www.linkedin.com/in/jugurta-maouchi-840b08227/'}><li className='linkedin-contact'><i className="fab fa-linkedin"></i></li></Link>
+            <Link to={'https://github.com/Jmaouchi'}><li className='github-contact'><i className="fab fa-github"></i></li></Link>
+          </div>
+        </div>
+
+        <div className='right-part'>
+        <Link to={'/work'}> <i className="fa-solid fa-circle-play"></i></Link>
+          <h5 className='heading-lets-talk'>Let's Talk Now</h5>
+          <h5 className='heading-add-review'>Add a review</h5>
+          <div className='side-paragraph'>
+            <p className='right-rotated-paragraph'>Noramohand</p>
+            <div className='noramohand'></div>
+          </div>
+        </div>  
+      </div>
+      </AnimatePage>
+    </div>   
+  )
+}
+
+export default About
