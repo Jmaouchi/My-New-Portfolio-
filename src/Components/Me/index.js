e5bef4e0e884ab6c63a32f7d4a09b702a500d82e
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-07-22 23:52:24 -0500

    added home page

---
+++ src/Components/Me/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import './style.css'
+import Footer from '../Footer'
+import myImage from '../../images/me-playing-2.jpg'
+import myImageTwo from '../../images/black-white.png'
+import AnimatePage from '../Animate'
+// import ModelOV from './ModelOV'
+
+function Me() {
+  return (
+ 
+    <div>
+            <AnimatePage>
+      <section className='format-section'>
+
+        <div className='dots'>
+          <div className='dots-display'>
+            <div className='dot red-dot'></div>
+            <div className='dot yellow-dot'></div>
+            <div className='dot green-dot'></div>
+          </div>
+          <p className='sign'>BeOnToP</p>
+          <div className='content-about-me'>
+            <img className='my-image' src={myImage} alt='my own img playing guitar'></img>
+            <div className='content-context'>
+              <h1 className='left-heading'>Hello,</h1>
+              <h2 className='inside-heading'>I am Jugurta</h2>
+              <p className='welcome-paragraph'>Welcome to my protfolio. my life is nothing without coding!</p>
+
+              <div className='section-two-about-me'>
+                <div className='about-me-content-one'>
+                  <p className='about-me-paragraph'>I'm here to show you most of my recent work, some of them are projects that I've built on my own, and some others are projects that I've learned by watching videos and   tried to build something simular without watching the code.                    tired to do the same on my own.
+                  well yea it was hard for me to build them but I'm proud of what I did.
+                  please check them out bellow</p>
+                </div>
+
+                <div className='about-me-content-two'>
+                  <img src={myImageTwo} alt='' className='my-image-two'></img>
+                  <p className='small-image-p-tag'>Small Town dude looking for adventure in the great wide somewhere.</p>
+                </div>
+              </div>
+            </div>
+          </div> 
+        </div>
+
+      </section>
+      </AnimatePage>
+    
+      <Footer></Footer>
+    </div>
+
+  )
+}
+
+export default Me
