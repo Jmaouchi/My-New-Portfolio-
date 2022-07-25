1643a98ad75da3fcdb1727ffd8c706ce85f9e143
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-07-25 00:16:59 -0500

    updated work layout

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Nav from '../../Components/Navbar/Nav'
 import './work.css'
-import image from '../../images/me.png'
 
 function Work() {
   return (
@@ -18,21 +17,28 @@ function Work() {
         </div>
         <div className="portfolio-items-wrapper">
           <div className="portfolio-item-wrapper">
-            <div className="portfolio-img-background one"></div>
+            <div className="portfolio-img-background one item"></div>
             <div className="img-text-wrapper">
               <div className="logo-wrapper">
-                <img src={image} alt=""></img>
+                <i class="fa-brands fa-html5"></i>
+                <i class="fa-brands fa-css3"></i>
+                <i class="fa-brands fa-js"></i>
               </div>
 
-              <div className="subtitle">Coding bootcamp and learning management system.</div>
+              <div className="subtitle">
+                <h2>Weather Dashboard</h2>
+                <p>an app that i've build on my own 2022,
+                  this app will give access to a daily and weekly wether status in your curent area
+                </p>
+              </div>
             </div>
           </div>
 
           <div className="portfolio-item-wrapper">
-            <div className="portfolio-img-background two"></div>
+            <div className="portfolio-img-background two item"></div>
             <div className="img-text-wrapper">
               <div className="logo-wrapper">
-                <img src={image} alt=""></img>
+
               </div>
 
               <div className="subtitle">Keep track of what you learn every day.</div>
@@ -40,10 +46,10 @@ function Work() {
           </div>
 
           <div className="portfolio-item-wrapper">
-            <div className="portfolio-img-background three"></div>
+            <div className="portfolio-img-background three item"></div>
             <div className="img-text-wrapper">
               <div className="logo-wrapper">
-                <img src={image} alt=""></img>
+               
               </div>
 
               <div className="subtitle">Coding bootcamp and learning management system.</div>
@@ -51,10 +57,10 @@ function Work() {
           </div>
 
           <div className="portfolio-item-wrapper">
-            <div className="portfolio-img-background four"></div>
+            <div className="portfolio-img-background four item"></div>
             <div className="img-text-wrapper">
               <div className="logo-wrapper">
-                <img src={image} alt=""></img>
+                
               </div>
 
               <div className="subtitle">Coding bootcamp and learning management system.</div>
@@ -62,10 +68,10 @@ function Work() {
           </div>
 
           <div className="portfolio-item-wrapper">
-            <div className="portfolio-img-background five"></div>
+            <div className="portfolio-img-background five item"></div>
             <div className="img-text-wrapper">
               <div className="logo-wrapper">
-                <img src={image} alt=""></img>
+                
               </div>
 
               <div className="subtitle">Coding bootcamp and learning management system.</div>
@@ -73,10 +79,10 @@ function Work() {
           </div>
 
           <div className="portfolio-item-wrapper">
-            <div className="portfolio-img-background six"></div>
+            <div className="portfolio-img-background six item"></div>
             <div className="img-text-wrapper">
               <div className="logo-wrapper">
-                <img src={image} alt=""></img>
+                
               </div>
 
               <div className="subtitle">I built the Quip Ecommerce platform, named a Top 25 Invention by Time Magazine in 2016.</div>
